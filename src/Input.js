a13967c2e9f072ce54a8242e22f337dce0552e2c
AtanasovCode <a_ljupce@yahoo.com>
2022-08-26 17:58:16 +0200

    Updated Layout and Style

--- src/Input.js
+++ src/Input.js
@@ -4,8 +4,12 @@ import React from 'react';
 function Input(props) {
     return (
         <label>
-            <div className="input-desc">{props.labelName}</div>
-            <input type={props.type} name={props.name} onChange={props.onChange} required />
+            <input 
+                type={props.type} 
+                name={props.name} 
+                onChange={props.onChange} 
+                placeholder={props.labelName}
+                required />
         </label>
     );
 }
